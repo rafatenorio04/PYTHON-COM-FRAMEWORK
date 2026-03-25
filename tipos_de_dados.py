72463d890391fa1f63f6617b55865a354303be4c
rafatenorio04 <rafaelatkj@gmail.com>
2026-03-24 21:58:01 -0300

    Add files via upload

--- tipos_de_dados.py
+++ tipos_de_dados.py
@@ -1,1 +1,32 @@
-print('hello world')
+# 4 tipos de dados em python
+
+
+# texto 
+
+
+'texto'
+"texto"
+
+
+# reais -  dinheiro - peso - altura - 
+
+
+10.6
+5.9
+21.0
+
+
+# inteiros
+
+
+5
+10
+5
+2
+
+
+# lógicos 
+
+
+True 
+False
